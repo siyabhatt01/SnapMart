9a50bf9b25aa30d09eb282763e6ad1ffd9e43aa8
siyabhatt <deepabhattnsr@gmail.com>
2025-01-14 16:15:07 +0530

    load data from product.js

--- scripts/sm.js
+++ scripts/sm.js
@@ -1,42 +1,11 @@
 
-const products=[
-    {
-       imageSource:'images/products/athletic-cotton-socks-6-pairs.jpg',
-       name :' Black and Gray Athletic Cotton Socks - 6 Pairs',
-       rating:{
-        stars:4.5,
-        count:87
-       },
-       price:1090
-    },
-    {
-        imageSource:'images/products/intermediate-composite-basketball.jpg',
-        name :'Intermediate size basketball',
-        rating:{
-         stars:4.0,
-         count:127
-        },
-        price:2095
-     },
-     {
-        imageSource:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name :'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating:{
-         stars:4.5,
-         count:56
-        },
-        price:799
-     },
-];
-
-
 let generatedProductsHTML='';
 
 products.forEach((products)=>{
   
     const productsHTML=`<div class="product-container">
             <div class="product-image-container">
-              <img class="product-image" src="${products.imageSource}">
+              <img class="product-image" src="${products.image}">
             </div>
   
             <div class="product-name limit-text-to-2-lines">
@@ -51,7 +20,7 @@ products.forEach((products)=>{
             </div>
   
             <div class="product-price">
-             $ ${(products.price/100).toFixed(2)}
+             $ ${(products.priceCents/100).toFixed(2)}
             </div>
   
             <div class="product-quantity-container">
